49d03073f07c7f2d077af4cad20d21fa558a3ae7
christrilogy <112494599+christrilogy@users.noreply.github.com>
2023-02-09 22:09:43 -0500

    Create banner.cpp

---
+++ banner.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include<stdio.h> 
+using namespace std;
+
+int main() {
+cout<< "******************************************************************" <<  endl; 
+cout << "February 3, 2023" <<  endl;  
+cout << "Christian Morgan" <<  endl;  
+cout << "CSC101" <<  endl;  
+cout << "Lab3\n" <<  endl;
+
+cout << "This lab is about basic arithematic operations and learning about what simple functions do." <<  endl;  
+  
+
+
+  string  name = "Bob";  
+  int age = 32; 
+  cout  <<  name  <<  " is "  <<  age  <<  "  years  old"  <<  endl;
+  string  last  =  "Smith"; 
+  cout  <<  name + last << endl; 
+  
+  printf("There are  %d  in a dozen and  %d  in a score\n", 12, 20); 
+
+
+  cout<< "******************************************************************" <<  endl; 
+  
+  return 0;
+}
